852b7509cba5e5146653f35492617d7a0b2e276f
Unknown <svezyridis@gmail.com>
2018-03-31 15:12:46 -0700

    working css and js in jsp

--- src/net/codejava/javaee/HelloServlet.java
+++ src/net/codejava/javaee/HelloServlet.java
@@ -28,10 +28,14 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String yourName = request.getParameter("yourName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password1");
+		
 		PrintWriter writer = response.getWriter();
-		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.println("<h1>Hello " + email +" "+ password+ "</h1>");
 		writer.close();
+		
+	}
 	}
 
-}
+
